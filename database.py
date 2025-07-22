0c71e2b5838b442c7ca6394aeca5bfd79fbd5dbd
zaaanpact0 <l.gaek@yandex.ru>
2025-07-22 16:13:34 +0300

    lf

--- database.py
+++ database.py
@@ -1,12 +1,20 @@
+import  os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./mydatabase.db"
+DATABASE_URL = os.getenv("", "sqlite:///./db.sqlite3")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- models.py
+++ models.py
@@ -1,21 +1,29 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from .database import Base
+from sqlalchemy.ext.associationproxy import association_proxy
+from database import Base
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    segments = relationship("UserSegment", back_populates="user")
+    name = Column(String, index=True)
+
+    user_segments = relationship("UserSegment", back_populates="user", cascade="all, delete-orphan")
+    segments = association_proxy("user_segments", "segment")
 
 class Segment(Base):
     __tablename__ = "segments"
-    slug = Column(String, primary_key=True, index=True)
-    description = Column(String, nullable=True)
-    users = relationship("UserSegment", back_populates="segment")
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+    user_segments = relationship("UserSegment", back_populates="segment", cascade="all, delete-orphan")
+    users = association_proxy("user_segments", "user")
 
 class UserSegment(Base):
-    __tablename__ = "user_segments"
-    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
-    segment_slug = Column(String, ForeignKey("segments.slug"), primary_key=True)
-    user = relationship("User", back_populates="segments")
-    segment = relationship("Segment", back_populates="users")
+    __tablename__ = "user_segment"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    segment_id = Column(Integer, ForeignKey("segments.id"))
+
+    user = relationship("User", back_populates="user_segments")
+    segment = relationship("Segment", back_populates="user_segments")
